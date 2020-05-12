11a8aa90ce8d56e6aef5071442ac70f5ae76bf4d
miahmo652 <miahmo652@users.noreply.github.com>
2020-05-12 19:32:26 -0400

    update

---
+++ team-builder/src/components/form.js
@@ -0,0 +1,62 @@
+import React, {useState} from "react";
+
+
+const Teamform = props =>{
+    const [title, settitle]=useState([
+     {  name: "",
+        email: "",
+        role: ""
+    }
+    ]);
+    const changehandler =(event) =>{
+        settitle({
+            ...title, [event.target.name]: event.target.value
+        })
+    }
+
+    
+    return(
+        <div className="container">
+    <h4>Add a team member</h4>
+        <form onSubmit={(event)=>{
+            event.preventDefault()
+        }}>
+            <label htmlFor="name">Name: </label>
+            <div className="form-group">
+            <input
+            type="text"
+            className="form-control"
+            name="name"
+            placeholder="Name.."
+            value={title.name}
+            onChange={changehandler}
+            />
+            </div>
+            <label htmlFor="email">Email: </label>
+            <div className="form-group">
+            <input 
+            type="text"
+            className="form-control"
+            name="email"
+            placeholder="Email.."
+            value={title.email}
+            onChange={changehandler}
+            />
+            </div>
+            <label htmlFor="role">Role: </label>
+            <div className="form-group">
+            <input 
+            type="text"
+            className="form-control"
+            name="role"
+            placeholder="Role.."
+            value={title.role}
+            onChange={changehandler}
+            />
+            </div>
+            <button type="submit">Click to submit</button>
+        </form>
+        </div>
+    )
+}
+export default Teamform;
